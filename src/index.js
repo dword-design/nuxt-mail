acaa14c000c9df255f352145c0baac750e9e3bcc
Sebastian Krüger <2pi_r2@gmx.de>
2021-03-28 15:01:40 +0200

    fix security

--- src/index.js
+++ src/index.js
@@ -4,15 +4,27 @@ import nodemailer from 'nodemailer'
 
 export default function (moduleOptions) {
   const options = { ...this.options.mail, ...moduleOptions }
+
   if (!options.smtp) {
     throw new Error('SMTP config is missing.')
   }
+
+  if(!options.mail || (!options.mail.to && !options.mail.cc && !options.mail.bcc)) {
+    throw new Error('You have to provide to/cc/bcc in config.')
+  }
+
   const app = express()
   const transport = nodemailer.createTransport(options.smtp)
   app.use(express.json())
   app.post('/send', async (req, res) => {
     try {
-      await transport.sendMail(req.body)
+      await transport.sendMail({
+        ...req.body,
+        to: [],
+        cc: [],
+        bcc: [],
+        ...options.mail
+      })
     } catch (error) {
       return res.status(500).send(error)
     }
