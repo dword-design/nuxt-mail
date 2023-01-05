5c552e4602a414d6daf5ad6e96d49a005f16fbe1
Sebastian Landwehr <sebastian.landwehr@honestly.com>
2023-01-05 15:55:41 +0100

    fix: foo

--- dist/plugin-nuxt2.js
+++
@@ -1,9 +0,0 @@
-export default ((context, inject) => inject('mail', {
-  send: async config => {
-    try {
-      await context.app.$axios.$post('/mail/send', config);
-    } catch (error) {
-      throw new Error(error.response.data);
-    }
-  }
-}));
