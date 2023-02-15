8073527e9f478e3053d22c91122ac97672c060d0
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-02-15 14:30:33 +0100

    fix: fix error handling for nuxt 3 (#145)

--- src/plugin-nuxt3.js
+++ src/plugin-nuxt3.js
@@ -1,13 +1,13 @@
-import { defineNuxtPlugin, useFetch } from '#app'
+import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin(() => ({
   provide: {
     mail: {
       send: async config => {
         try {
-          await useFetch('/mail/send', { body: config, method: 'POST' })
+          await $fetch('/mail/send', { body: config, method: 'POST' })
         } catch (error) {
-          throw new Error(error.response.data)
+          throw new Error(error.response._data.statusMessage)
         }
       },
     },
