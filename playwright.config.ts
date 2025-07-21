51685b2142fe08df411714ed5e2fc6255a6801a6
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-07-21 21:28:30 +0200

    fix: foo

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   fullyParallel: true,
-
   preserveOutput: 'failures-only',
 
   snapshotPathTemplate:
     '{snapshotDir}/{testFileDir}/{testFileName}-snapshots/{arg}{-projectName}{ext}',
+
+  timeout: 60_000,
 });
