cd1ece3f1d4e071b53bced5c7d084992d49bba25
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-12-26 16:23:27 +0100

    fix: fix message types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,24 +1,26 @@
 import type { SendMailOptions } from 'nodemailer';
 import type SMTPTransport from 'nodemailer/lib/smtp-transport';
+import type { RequireAtLeastOne } from 'type-fest';
+
+export type Message = RequireAtLeastOne<
+  SendMailOptions,
+  'to' | 'cc' | 'bcc'
+> & { name?: string };
 
 export interface MailOptions {
   message: Message[];
   smtp: SMTPTransport.Options | null;
 }
 
-export type Message = Omit<SendMailOptions, 'to' | 'cc' | 'bcc'> &
-  (
-    | { to: SendMailOptions['to'] }
-    | { cc: SendMailOptions['cc'] }
-    | { bcc: SendMailOptions['bcc'] }
-  ) & { name?: string };
-
 export interface MailOptionsInput extends Omit<MailOptions, 'message'> {
   message: MailOptions['message'] | Message;
   smtp: SMTPTransport.Options;
 }
 
-export interface MessageWithConfig extends Message {
+export interface MessageWithConfig extends Omit<
+  SendMailOptions,
+  'to' | 'cc' | 'bcc'
+> {
   config: number;
 }
 
